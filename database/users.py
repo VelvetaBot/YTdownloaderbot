01a7a3902f8bf3bf6d1b496eacd266234cc8c727
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2025-07-22 20:21:06 +0530

    users.py

--- database/users.py
+++ database/users.py
@@ -1,1 +1,25 @@
+from datetime import datetime
+from pymongo import MongoClient
+from config import MONGO_URL
+
+client = MongoClient(MONGO_URL)
+db = client.ytdl_bot
+users = db.users
+
+async def add_user(user):
+    if not users.find_one({"_id": user.id}):
+        users.insert_one({
+            "_id": user.id,
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name or "",
+            "join_date": datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S"),
+            "downloads_today": 0,
+        })
+
+async def get_user_data(user_id):
+    return users.find_one({"_id": user_id})
+
+async def increment_download(user_id):
+    users.update_one({"_id": user_id}, {"$inc": {"downloads_today": 1}})
 
