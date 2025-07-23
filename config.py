9f26ea53743130554d0e753f3c608b88a2deaaf0
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2025-07-23 20:47:16 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-API_ID = 883128927
-API_HASH = "your_api_hash"
-BOT_TOKEN = "7523588106:AAHLLbwPCLJwZdKUVL6gA6KNAR_86eHJCWU"
-MONGO_URL = "your_mongodb_url"  # You can use MongoDB Atlas
+import os
 
+API_ID = int(os.getenv("11253846"))
+API_HASH = os.getenv("8db4eb50f557faa9a5756e64fb74a51a")
+BOT_TOKEN = os.getenv("7523588106:AAHLLbwPCLJwZdKUVL6gA6KNAR_86eHJCWU")
+MONGO_URL = os.getenv("MONGO_URL")
