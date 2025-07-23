a74b1ccc81b6858edbdd4579f3a1ea09d3eb5c56
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2025-07-23 20:16:32 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID = 12345678
+API_ID = 883128927
 API_HASH = "your_api_hash"
 BOT_TOKEN = "7523588106:AAHLLbwPCLJwZdKUVL6gA6KNAR_86eHJCWU"
 MONGO_URL = "your_mongodb_url"  # You can use MongoDB Atlas
