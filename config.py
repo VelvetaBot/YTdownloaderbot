164c41e43b84534588bee6a6eba9e5f7de722cba
VelvetaBot <108663991+VelvetaBot@users.noreply.github.com>
2025-07-23 21:36:48 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.getenv("883128927"))
+API_ID = int(os.getenv("11253846"))
 API_HASH = os.getenv("8db4eb50f557faa9a5756e64fb74a51a")
 BOT_TOKEN = os.getenv("7523588106:AAHLLbwPCLJwZdKUVL6gA6KNAR_86eHJCWU")
